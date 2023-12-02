e30f4e5a37f9fdf204953127d175d6675eaf9a06
bhagyaj <bhagyajkumar1@gmail.com>
2023-12-02 23:03:36 +0530

    generates the required tables

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from utils import InvalidMacroException, split_instruction
-import pprint
-
+from tabulate import tabulate
 
 class MacroProcessor():
 
@@ -16,7 +15,7 @@ class MacroProcessor():
         self.processed_content = processed
         self.def_tab = []
         self.name_tab = []
-        self.output_tab = []
+        self.output_table = []
         self.current_address = 0
 
 
@@ -24,7 +23,10 @@ class MacroProcessor():
         if len(self.processed_content) == 0:
             return None
         self.current_address += 1
-        return self.processed_content.pop(0)
+        
+        content =  self.processed_content.pop(0)
+        self.output_table.append(content)
+        return content
 
     def process_macro(self):
         current_line = self.get_line()
@@ -38,7 +40,7 @@ class MacroProcessor():
                 self.fill_deftab(current_line["label"])
             else:
                 current_line["index"] = self.current_address
-                self.output_tab.append(current_line)
+                self.def_tab.append(current_line)
             print(current_line)
             current_line = self.get_line()
 
@@ -47,7 +49,7 @@ class MacroProcessor():
         print("processing macro")
         name_tab_content = {
             "name" : macro_name,
-            "start": self.current_address,
+            "start": self.current_address + 1,
             "end" : None
         }
         processing = True
@@ -56,7 +58,7 @@ class MacroProcessor():
             if current_line is None:
                 raise InvalidMacroException
             elif current_line["opcode"].lower() == "endmacro":
-                name_tab_content["end"] = self.current_address
+                name_tab_content["end"] = self.current_address - 1
                 processing = False
             else:
                 current_line["index"] = self.current_address
@@ -66,16 +68,58 @@ class MacroProcessor():
         print("Macro Processing Finished")
 
 
+    def display_deftab(self):
+        tabular_data = []
+        for i in self.def_tab:
+            tabular_data.append(
+                (i["index"], i["label"], i["opcode"], ",".join(x for x in i["operands"] ))
+            )
+        table = tabulate(
+            headers=["address", "label", "opcode", "operands"],
+            tabular_data=tabular_data,
+            tablefmt="fancy_grid"
+        )
+        print(table)
+
+
+    def display_nametab(self):
+        tabular_data = []
+        for i in self.name_tab:
+            tabular_data.append(
+                (i["name"], i["start"], i["end"])
+            )
+        table = tabulate(
+            headers = ["name", "start", "end"],
+            tabular_data = tabular_data,
+            tablefmt="fancy_grid"
+        )
+        print(table)
+
+    def display_output_tab(self):
+        tabular_data = []
+        for i in self.output_table:
+            tabular_data.append(
+                (i["label"], i["opcode"], ",".join(x for x in i["operands"]))
+            )
+
+        table = tabulate(
+            headers = ("label", "opcode", "operands"),
+            tabular_data=tabular_data,
+            tablefmt="fancy_grid"
+        )
+        print(table)
+
 
 
 def main():
     macroprocessor = MacroProcessor("PGM.ASM")
     macroprocessor.process_macro()
     print("Deftab")
-    pprint.pprint(
-        macroprocessor.def_tab
-    )
-    print()
+    macroprocessor.display_deftab()
+    print("NameTab")
+    macroprocessor.display_nametab()
+    print("OutputTab")
+    macroprocessor.display_output_tab()
 
 if __name__ == "__main__":
     main()
